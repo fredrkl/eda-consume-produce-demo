b53c4ea96b441db7869b24949ebd453023a89ced
Fredrik Klingenberg <fredrkl@gmail.com>
2025-10-28 12:04:23 +0100

    ✨ Kafka cluster

--- eda-api/Program.cs
+++ eda-api/Program.cs
@@ -1,11 +1,44 @@
+using KafkaFlow;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+// KafkaFlow
+builder.Services.AddKafka(kafka => {
+  kafka.AddCluster(cluster => {
+    cluster.WithBrokers(["kafka-14f487f0-fredrkl-0955.k.aivencloud.com:14350"]);
+    cluster.WithSecurityInformation(security => {
+      security.SecurityProtocol = KafkaFlow.Configuration.SecurityProtocol.Ssl;
+      security.SslCaPem = builder.Configuration["KAFKA_CA_CERTIFICATE"];
+      security.SslCertificatePem = builder.Configuration["KAFKA_ACCESS_CERTIFICATE"];
+      security.SslKeyPem = builder.Configuration["KAFKA_ACCESS_KEY"];
+    });
+  });
+});
 
+//        return services.AddKafka(kafka => kafka
+//            .UseMicrosoftLog()
+//            .AddCluster(cluster => cluster
+//                .WithBrokers([kafkaConfiguration.ServiceUri])
+//                .WithSecurityInformation(h =>
+//                {
+//                    h.SecurityProtocol = SecurityProtocol.Ssl;
+//                    h.SslCaPem = kafkaConfiguration.CaCertificate;
+//                    h.SslCertificatePem = kafkaConfiguration.AccessCertificate;
+//                    h.SslKeyPem = kafkaConfiguration.AccessKey;
+//                })
+//                .WithSchemaRegistry(a =>
+//                {
+//                    a.Url = kafkaConfiguration.SchemaUrl;
+//                    a.BasicAuthUserInfo = kafkaConfiguration.SchemaBasicAuth;
+//                })
+//                .AddProducer(Constants.ProducerName, kafkaConfiguration.Topic)));
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
